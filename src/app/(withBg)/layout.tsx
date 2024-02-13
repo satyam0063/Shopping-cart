ce2f08587ecc0fa2793b16b9cf0c51ecb07a46d5
Sidd1829 <siddhantlokhandecmpn@gmail.com>
2024-02-14 01:06:48 +0530

    Header update

--- src/app/(withBg)/layout.tsx
+++ src/app/(withBg)/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import Header from "../components/UI/Header";
 
 const WithBglayout = ({
@@ -7,10 +8,18 @@ const WithBglayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div className="w-full">
-      <Header />
-      {children}
-    </div>
+    <>
+      <div className="w-full inline-flex h-screen">
+        <Image
+          src="/static/images/home/home-slider1.jpg"
+          layout="fill"
+          alt="logo"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-[#4f4f4f] to-[#272c41] opacity-80 transition-all duration-300"></div>
+        <Header />
+      </div>
+      <div className="w-full inline-flex">{children}</div>
+    </>
   );
 };
 
--- src/app/components/UI/CartButton.tsx
+++ src/app/components/UI/CartButton.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+'use client'
+import React from "react";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
 
 const CartButton = () => {
   return (
-    <div>CartButton</div>
-  )
-}
+    <div className="relative inline-block">
+      <button>
+        <ShoppingBagIcon className="h-6 w-6 text-white" />
+      </button>
+      <span className="absolute -right-3 -top-3 inline-flex items-center justify-center h-5 w-5 bg-white text-black text-xs font-bold rounded-full">
+        3
+      </span>
+    </div>
+  );
+};
 
-export default CartButton
+export default CartButton;
