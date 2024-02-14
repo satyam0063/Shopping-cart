7bdad879a66f71d0d541f603e38da2d10f7354f6
satyam0063 <satyamdash29@gmail.com>
2024-02-14 14:41:32 +0530

    quotation section

--- src/app/(withBg)/home/page.tsx
+++ src/app/(withBg)/home/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import ImageGrid from "@/app/components/Home/ImageGrid";
+import Quote from "@/app/components/Home/Quote";
 import React from "react";
 
 const Home = () => {
   return (
     <>
       <ImageGrid />
+      <Quote/>
     </>
   );
 };
---
+++ src/app/components/Home/Quote.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Quote = () => {
+  return (
+    <div className="pt-[70px] pb-[100px] ">
+      <div className="max-w-[1200px] mr-auto ml-auto">
+        <div className="flex flex-col mr-[35%]">
+          <div className="text-[40px] mb-5">
+            <i className="fa fa-quote-left  text-[#54595f]" />
+          </div>
+          <div>
+            <h3 className="leading-tight m-0 text-[#000] text-[30px] font-normal">
+              Proident Earum, Luctus Autem, Class Impedit Nostrud Pariatur,
+              Perferendis Eiusmod Minima Perferendis, Itaque Recusandae Eligendi
+              Facilisis.
+            </h3>
+          </div>
+          <div className="mt-7">
+            <div className="flex items-center w-[300px] text-[#4f4f4f]">
+            <span className=" top-0 left-0 w-full h-full border-t border-dotted border-gray-500"></span>
+            <span className=" text-nowrap  text-[14px] ml-2 font-sans">Mark Fosters</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Quote;
