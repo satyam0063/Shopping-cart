afe82f7698a94f55355f92f01a9665dbd790baa4
Sidd1829 <siddhantlokhandecmpn@gmail.com>
2024-02-16 17:15:50 +0530

    Featured and Recommended Products

--- src/app/(withBg)/home/page.tsx
+++ src/app/(withBg)/home/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import ImageGrid from "@/app/components/Home/ImageGrid";
 import OfferSection from "@/app/components/Home/OfferSection";
-import ProductList from "@/app/components/Home/ProductList";
+import ProductList from "@/app/components/Product/ProductList";
 import Quote from "@/app/components/Home/Quote";
 import TrackingBox from "@/app/components/UI/TrackingBox";
 import React from "react";
@@ -13,10 +13,10 @@ const Home = () => {
       <Quote />
       <OfferSection />
       <div className="max-w-[1200px] mx-auto mt-20">
-        <h2 className="text-center uppercase font-barlow-condensed text-[42px] mb-8">
+        {/* <h2 className="text-center uppercase font-barlow-condensed text-[42px] mb-8">
           Featured Products
-        </h2>
-        <ProductList />
+        </h2> */}
+        <ProductList title="FEATURED PRODUCTS" productListType='featured' data={[]} />
       </div>
       <TrackingBox />
     </div>
---
+++ src/app/components/Product/ProductList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ProductCard from "../UI/ProductCard";
+
+const ProductList = (props: any) => {
+  const array = [1, 2, 3, 4, 5];
+  return (
+    <>
+      <h2 className={`${props.productListType ? 'text-center' : 'text-start'} uppercase font-barlow-condensed text-[42px] mb-8`}>
+        {props.title}
+      </h2>
+      <div className="w-full flex flex-row flex-wrap">
+        {array.map((_item: any, idx: any) => (
+          <div className="w-full sm:w-1/4 px-2 py-2" key={idx}>
+            <ProductCard />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
