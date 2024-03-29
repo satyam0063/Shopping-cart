77857c5c6a530c7dfa3e8e86714174ff8775d317
satyam0063 <satyamdash29@gmail.com>
2024-03-30 00:07:26 +0530

    test

--- src/app/(withOutBg)/cart/page.tsx
+++ src/app/(withOutBg)/cart/page.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/jsx-no-undef */
 "use client";
+/* eslint-disable react/jsx-no-undef */
 import React, { useCallback, useMemo } from "react";
 import Image from "next/image";
 import { UseAppSelector } from "@/redux/store";
--- src/app/(withOutBg)/checkout/page.tsx
+++ src/app/(withOutBg)/checkout/page.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/jsx-no-undef */
 "use client";
+/* eslint-disable react/jsx-no-undef */
 import React from "react";
 
 import LeftSection from "@/app/components/Checkout/LeftSection";
