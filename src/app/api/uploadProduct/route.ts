5bab84a7c6eaa52a6f9236f246e613c1768f865b
satyam0063 <satyamdash29@gmail.com>
2024-03-31 01:47:13 +0530

    Update route.ts

--- src/app/api/uploadProduct/route.ts
+++ src/app/api/uploadProduct/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
     dataWithsignedUrl = await Promise.all(
       data.map(async (item) => {
         const response = await axios.post(
-          `${process.env.API_PATH}/api/viewFile`,
+          `https://shopping-cart-kappa-dusky.vercel.app/api/viewFile`,
           item
         ); // it coverts all thumbnail urls to pre-signed urls
         item.thumbnail = response.data.url;
